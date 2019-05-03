d87b997f6660ab394228d0842db9089a474aab40
Pedro Arantes <arantespp@gmail.com>
2019-05-03 15:47:58 -0300

    Updates

---
+++ precache-manifest.d9301a7c5af27219950e175102fe9a93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea27f4634908e27ebd25db2a3490dc4",
+    "url": "/multimapas/index.html"
+  },
+  {
+    "revision": "03839e65908fc3347735",
+    "url": "/multimapas/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/multimapas/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "03839e65908fc3347735",
+    "url": "/multimapas/static/js/main.3a764348.chunk.js"
+  },
+  {
+    "revision": "63436f8e6b6b829f7dd0",
+    "url": "/multimapas/static/js/runtime~main.36f6b9d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/multimapas/static/media/logo.5d5d9eef.svg"
+  }
+]);
